7a57a0182747b614501925999f00a9ba2e702fbf
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-04-29 18:27:29 +0900

    4주차 과제

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    def __str__ (self): #입력한 데이터가 호출되면 데이터의 title이 대표값으로 나오게 함
+        return self.title
+    
+    def summary(self):
+        return self.body[:50]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
-
 def mainpage(request):
-    return render(request, 'main/mainpage.html')
+    blogs = Blog.objects.all()#blogs 변수에 모든 Blog 객체 저장(모든 게시물 저장)
+    return render(request, 'main/mainpage.html',{'blogs':blogs})
 
 def secondpage(request):
     return render(request, 'main/secondpage.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.pub_date = timezone.now()
+    new_blog.body = request.POST['body']
+
+    new_blog.save()
+
+    return redirect('detail', new_blog.id)#여기서 detail은 뭘까?
+
+def new(request):#어디에서 호출되는지?
+    return render(request, 'main/new.html')
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'main/detail.html',{'blog':blog})#중괄호 안에 blog는 뭘까
